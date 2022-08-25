6c2a5b193bb8a27ee19198043718ea8e17171277
Crackbite <94061798+Crackbite@users.noreply.github.com>
2022-08-25 22:38:40 +0500

    Reworked the player movement logic

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,12 +8,13 @@ public class Movement : MonoBehaviour
     [SerializeField] private float _delayAfterLanding = 1f;
     [SerializeField] private float _speed = 18f;
 
-    private bool _canMove;
-    private float _currentSpeed;
-    private Platform _lastAffectedPlatform;
-
+    private bool _isMovementEnabled;
+    private Platform _nextPlatform;
+    private Vector3 _nextPosition;
     private Player _player;
     private PlayerAnimator _playerAnimator;
+
+    private bool CanMove => _nextPosition != default && transform.position != _nextPosition;
 
     public void MoveBack()
     {
@@ -48,27 +49,18 @@ public class Movement : MonoBehaviour
 
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.TryGetComponent(out Platform platform))
+        if (collision.TryGetComponent(out Platform platform) && platform == _nextPlatform)
         {
-            _lastAffectedPlatform = platform;
+            _playerAnimator.RunFlightToIdle();
         }
-
-        if (Math.Abs(_currentSpeed - _speed) > 0 || collision.TryGetComponent(out Roof _) == false)
-        {
-            return;
-        }
-
-        _currentSpeed = 0;
-        _playerAnimator.RunFlightToIdle();
-
-        PlaceInPlatformCenter();
-
-        _canMove = true;
     }
 
     private void Update()
     {
-        transform.Translate(Vector3.forward * _currentSpeed * Time.deltaTime);
+        if (CanMove)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _nextPosition, _speed * Time.deltaTime);
+        }
     }
 
     private void OnDisable()
@@ -79,15 +71,7 @@ public class Movement : MonoBehaviour
     private IEnumerator EnableMovement()
     {
         yield return new WaitForSeconds(_delayAfterLanding);
-        _canMove = true;
-    }
-
-    private bool IsWall()
-    {
-        var ray = new Ray(transform.position, transform.forward);
-        float maxDistance = _lastAffectedPlatform.GetComponent<Collider>().bounds.size.x;
-
-        return Physics.Raycast(ray, maxDistance);
+        _isMovementEnabled = true;
     }
 
     private void Landing_OnLanded()
@@ -97,27 +81,73 @@ public class Movement : MonoBehaviour
 
     private void Move(float degrees)
     {
-        if (_canMove == false)
+        if (_isMovementEnabled == false || CanMove)
         {
             return;
         }
 
         transform.localEulerAngles = new Vector3(0, degrees, 0);
 
-        if (IsWall())
+        if (TryGetNextPlatform(out Platform nextPlatform) == false)
         {
             return;
         }
 
-        _canMove = false;
-
         _playerAnimator.RunIdleToFlight();
-        _currentSpeed = _speed;
+
+        Vector3 platformCenter = nextPlatform.GetComponent<Collider>().bounds.center;
+        _nextPosition = new Vector3(platformCenter.x, transform.position.y, platformCenter.z);
+
+        _nextPlatform = nextPlatform;
     }
 
-    private void PlaceInPlatformCenter()
+    private bool TryGetNextPlatform(out Platform platform)
     {
-        Vector3 platformCenter = _lastAffectedPlatform.GetComponent<Collider>().bounds.center;
-        transform.position = new Vector3(platformCenter.x, transform.position.y, platformCenter.z);
+        const float RaycastDepth = .5f;
+        const int RaycastHitBuffer = 128;
+
+        platform = null;
+
+        Vector3 position = transform.position;
+        position.y -= RaycastDepth;
+
+        var raycastAllHits = new RaycastHit[RaycastHitBuffer];
+        int hits = Physics.RaycastNonAlloc(position, transform.forward, raycastAllHits, Mathf.Infinity);
+
+        if (hits < 1)
+        {
+            return false;
+        }
+
+        var raycastHits = new RaycastHit[hits];
+        Array.Copy(raycastAllHits, raycastHits, hits);
+        Array.Sort(raycastHits, (x, y) => x.distance.CompareTo(y.distance));
+
+        Platform requiredPlatform = null;
+
+        for (var i = 0; i < hits; i++)
+        {
+            RaycastHit raycastHit = raycastHits[i];
+
+            if (raycastHit.collider.TryGetComponent(out Platform currentPlatform))
+            {
+                requiredPlatform = currentPlatform;
+                continue;
+            }
+
+            if (raycastHit.collider.TryGetComponent(out Roof _))
+            {
+                break;
+            }
+        }
+
+        if (requiredPlatform == null)
+        {
+            return false;
+        }
+
+        platform = requiredPlatform;
+
+        return true;
     }
 }
