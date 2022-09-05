4136eaf0d4a253261c599378de22463e5955de53
Crackbite <94061798+Crackbite@users.noreply.github.com>
2022-09-05 23:19:53 +0500

    Added impulse when the player lands

---
+++ Assets/Scripts/Impulse.cs
@@ -0,0 +1,25 @@
+using Cinemachine;
+using UnityEngine;
+
+[RequireComponent(typeof(CinemachineImpulseSource))]
+public class Impulse : MonoBehaviour
+{
+    private CinemachineImpulseSource _cinemachineImpulse;
+    private bool _isActivated;
+
+    private void Start()
+    {
+        _cinemachineImpulse = GetComponent<CinemachineImpulseSource>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (_isActivated || collision.TryGetComponent(out Player _) == false)
+        {
+            return;
+        }
+
+        _isActivated = true;
+        _cinemachineImpulse.GenerateImpulse();
+    }
+}
